649c25a21d1ad06e213b88067d767e3aeb24d3ee
Rahul saha <rahulsaha87928@gmail.com>
2023-10-01 13:42:50 +0600

    assignment operator

---
+++ src/AssignmentOperator/AssignmentOperator.java
@@ -0,0 +1,20 @@
+package AssignmentOperator;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 2;
+
+        x += y;
+        System.out.println("X = " + x);
+        x -= y;
+        System.out.println("X = " + x);
+        x *= y;
+        System.out.println("X = " + x); 
+        x /= y;
+        System.out.println("X = " + x); 
+        x %= y;
+        System.out.println("X = " + x); 
+    }
+
+}
