95e15e1cd63cb5460dbf472d3f81fca07d0fd0a5
Rahul saha <rahulsaha87928@gmail.com>
2023-10-03 10:53:09 +0600

    break and continue demo

---
+++ src/BreakDemo/BreakDemo.java
@@ -0,0 +1,19 @@
+package BreakDemo;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i == 10) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            if (i == 10) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
