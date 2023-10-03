6fe5317ce29efb73d2e0bbdf4a5e08257657281f
Rahul saha <rahulsaha87928@gmail.com>
2023-10-03 10:45:50 +0600

    for loop customized

--- src/ForLoopDemo/ForLoopDemo.java
+++ src/ForLoopDemo/ForLoopDemo.java
@@ -11,5 +11,11 @@ public class ForLoopDemo {
         for (int i = 1; i <= 50; i = i + 2) {
             System.out.println(i);
         }
+
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum = sum + i;
+            System.out.println(sum);
+        }
     }
 }
