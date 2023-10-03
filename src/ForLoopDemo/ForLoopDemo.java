ddf4d715ab60122ed486a14a2b0628ba3fef8afc
Rahul saha <rahulsaha87928@gmail.com>
2023-10-03 10:22:59 +0600

    for loop demo

---
+++ src/ForLoopDemo/ForLoopDemo.java
@@ -0,0 +1,15 @@
+package ForLoopDemo;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello Bro");
+        }
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i + " Hello Bro");
+        }
+        for (int i = 1; i <= 50; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
